1c10e5d6ff25d4383ce69ce0d33adb0bd46c2284
lingeshwaran05 <lingeshkrish08@gmail.com>
2023-03-08 09:17:14 +0530

    temporary

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ let searchBtn = document.getElementById("search-btn");
 let result = document.getElementById("result");
 
 //function to fetch data from api
-let key = "7f9a3b82";
+
 let getMovie = () => {
     let movieName = movieNameRef.value;
-    let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
+    let key = "7f9a3b82";
+    let url = `http://www.omdbapi.com/?t=${movieName}&apikey="${key}"`;
     //if input field is empty
 
     if (movieName.length <= 0) {
